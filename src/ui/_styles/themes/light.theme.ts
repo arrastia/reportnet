f6e5a1a50bb76b9cd6f7563b56524a76f11be403
Arrastia <arrastia@pm.me>
2021-12-03 11:15:16 +0100

    ADD - light theme palette

---
+++ src/ui/_styles/themes/light.theme.ts
@@ -0,0 +1,36 @@
+export const light = {
+  colors: {
+    background: 'linear-gradient(#2396bf, #0d5b77 40%, #004961 3%, #004961)',
+    blue: '#005e9c',
+    borderColor: 'rgb(255 255 255 / 35%)',
+    glass: 'rgb(255 255 255 / 31%)',
+    green: '#009490',
+    text: '#282c34',
+    themeColor: '#3c3a3a'
+  }
+};
+
+// --bg: white;
+//   --theme-bg-color: rgb(255 255 255 / 31%);
+//   --theme-color: #3c3a3a;
+//   --inactive-color: #333333;
+//   --button-inactive: #3c3a3a;
+//   --search-bg: rgb(255 255 255 / 31%);
+//   --dropdown-bg: #f7f7f7;
+//   --overlay-bg: rgb(255 255 255 / 30%);
+//   --dropdown-hover: rgb(236 236 236);
+//   --border-color: rgb(255 255 255 / 35%);
+//   --popup-bg: rgb(255 255 255);
+//   --hover-menu-bg: rgba(255 255 255 / 35%);
+//   --scrollbar-bg: rgb(255 253 253 / 57%);
+//   --content-title-color: --theme-color;
+//   --large-title-bg: rgb(255 255 255 / 30%);
+//   --dialog-bg: rgb(255 255 255 / 80%);
+
+//   --color-accent-main: #50a2e9;
+//   --primary-color: #50a2e9;
+
+//   --theme-bg-color-dialog: rgb(255 255 255 / 50%);
+//   --theme-bg-color-input: rgb(255 255 255 / 100%);
+
+//   --background-effect: linear-gradient(180deg, rgba(255, 255, 255, 0.72) 0%, rgba(255, 255, 255, 0.45) 100%);
