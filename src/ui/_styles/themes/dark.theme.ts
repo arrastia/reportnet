ca29507eb500fd0133d108e4352b7e3307fced50
Arrastia <arrastia@pm.me>
2021-12-03 11:15:05 +0100

    ADD - dark theme palette

---
+++ src/ui/_styles/themes/dark.theme.ts
@@ -0,0 +1,32 @@
+export const dark = {
+  colors: {
+    background: '#282c34',
+    blue: '#005e9c',
+    borderColor: 'rgba(113 119 144 / 25%)',
+    glass: 'rgba(16 18 27 / 40%)',
+    green: '#009490',
+    text: '',
+    themeColor: '#f9fafb'
+  }
+};
+
+// --bg: white;
+//   --theme-bg-color: rgba(16 18 27 / 40%);
+//   --border-color: rgba(113 119 144 / 25%);
+//   --theme-color: #f9fafb;
+//   --inactive-color: rgb(113 119 144 / 78%);
+//   --body-font: 'Poppins', sans-serif;
+//   --hover-menu-bg: rgba(12 15 25 / 30%);
+//   --content-title-color: #999ba5;
+//   --content-bg: rgb(146 151 179 / 13%);
+//   --button-inactive: rgb(249 250 251 / 55%);
+//   --dropdown-bg: #21242d;
+//   --dropdown-hover: rgb(42 46 60);
+//   --popup-bg: rgb(22 25 37);
+//   --search-bg: #14162b;
+//   --overlay-bg: rgba(36, 39, 59, 0.3);
+//   --scrollbar-bg: rgb(1 2 3 / 40%);
+//   --large-title-bg: rgb(255 255 255 / 30%);
+//   --dialog-bg: rgb(255 255 255 / 70%);
+
+//   --background-effect: var(--transparent);
