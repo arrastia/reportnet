3c3c81b9568af07167e28cd6f77dcf90c7348512
Arrastia <arrastia@pm.me>
2022-01-10 17:05:50 +0100

    ADD - rainbow logo

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,6 +1,7 @@
 import logo from './logo.png';
 import memoji from './memoji.png';
+import rainbow_icon from './rainbow-icon.png';
 import reactLogo from './reactLogo.svg';
 import reportnetLogo from './reportnetLogo.svg';
 
-export { logo, memoji, reactLogo, reportnetLogo };
+export { logo, memoji, rainbow_icon, reactLogo, reportnetLogo };
