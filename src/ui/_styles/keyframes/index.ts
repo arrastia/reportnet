987875f69699085e1f2c426a6595ce4a2ee10cf2
Arrastia <arrastia@pm.me>
2022-01-22 18:10:10 +0100

    ADD - zoom animation

---
+++ src/ui/_styles/keyframes/index.ts
@@ -0,0 +1,6 @@
+import { fade } from './fade';
+import { slideDown } from './slideDown';
+import { slideUp } from './slideUp';
+import { zoom } from './zoom';
+
+export { fade, slideDown, slideUp, zoom };
