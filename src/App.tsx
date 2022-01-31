733bd20d5dfe9954d9f0a5bbf9c27d9794a23f76
Arrastia <arrastia@pm.me>
2022-01-31 23:21:27 +0100

    UPDATE - App routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,46 @@
-import './App.css';
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
 
-import { reactLogo } from 'assets/images';
+import { ThemeProvider } from 'styled-components';
+
+import { GlobalStyles } from './Global.styles';
+
+import { routes } from 'configuration/routes';
+
+import { Layout } from 'ui/_components/Layout';
+
+import { base, theme } from 'ui/_styles/themes';
+
+import { Home } from 'ui/Home';
+import { PublicHome } from 'ui/PublicHome';
+import { UserSettings } from 'ui/UserSettings';
+
+import { themeStore, tokenStore } from 'ui/_tools/Atoms/UserStore';
 
 export const App = () => {
+  const isDarkMode = useRecoilValue(themeStore);
+  const token = useRecoilValue(tokenStore);
+
+  const renderRouting = () => {
+    if (!token) return <Route component={PublicHome} exact path={routes.DATAFLOWS} />;
+
+    return (
+      <Fragment>
+        <Route component={Home} exact path={routes.DATAFLOWS} />
+        <Route component={UserSettings} exact path={routes.USER_SETTINGS} />
+      </Fragment>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img alt="logo" className="App-logo" src={reactLogo} />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={{ ...base, ...theme[isDarkMode ? 'dark' : 'light'] }}>
+      <GlobalStyles />
+      <Router>
+        <Layout>
+          <Switch>{renderRouting()}</Switch>
+        </Layout>
+      </Router>
+    </ThemeProvider>
   );
 };
